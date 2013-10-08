327c059a0a722f7af5af5d0feeb1c2eeb39e0ea1
Andre Jones <andreij6@gmail.com>
2013-10-08 12:56:36 -0500

    few changes

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,18 @@
 module WelcomeHelper
+
+  def skills
+    return ['Ruby on Rails','Relational Databases','Jquery', 'HTML', 'Javascript','Object-Oriented Design','SQL', 'GIS']
+  end
+  
+  
+  def works
+    return{
+              "Github" => 'http://github.com/andreij6', 
+              "Lynda Certificates" => 'http://www.lynda.com/AllCertificates/User/1778196',
+              "Ruby Monk" => 'https://rubymonk.com/u/andre-jones',
+              "Code Academy" => 'http://www.codecademy.com/andreij6',
+         }
+    end
+    
+   
 end
